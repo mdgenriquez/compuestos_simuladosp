1e9def15dfd52a704da195364f15ce0fd2bc1443
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2025-01-28 14:20:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,13 +23,6 @@ st.sidebar.markdown("Autor:\n- Dr. Jesus Alvarado\n- Bach. Guadalupe Enriquez")
 
 # Streamlit Title
 st.title("COMPUESTOS SIMULADOS")
+
 def img_to_base64(image_path):
     """Convert image to base64."""
-    try:
-        with open(image_path, "rb") as img_file:
-            return base64.b64encode(img_file.read()).decode()
-    except Exception as e:
-        logging.error(f"Error converting image to base64: {str(e)}")
-        return None
-
-@st.cache_data(show_spinner=False)
